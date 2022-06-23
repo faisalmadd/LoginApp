23fedc960ab93edda84bfb35143eef077158446b
faisalmadd <84001348+faisalmadd@users.noreply.github.com>
2022-06-23 15:47:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.loginapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView login = (TextView)findViewById(R.id.lnkRegister);
+        login.setMovementMethod(LinkMovementMethod.getInstance());
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
